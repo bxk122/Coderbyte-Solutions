f1fa8721c4fa5ff783f19fc5f25be00124559a29
Badal Karki <53354741+bxk122@users.noreply.github.com>
2020-02-17 18:24:18 -0500

    Update CoderByteSolution.java

--- CoderByteSolution.java
+++ CoderByteSolution.java
@@ -1,16 +1,14 @@
-Have the function FirstReverse(str) take the str parameter being passed and return
-the string in reversed order. For example: if the input string is "Hello World and Coders" 
-then your program should return the string sredoC dna dlroW olleH
-
+// Have the function FirstReverse(str) take the str parameter being passed and return
+// the string in reversed order. For example: if the input string is "Hello World and Coders" 
+// then your program should return the string sredoC dna dlroW olleH
 import java.util.*; 
 import java.io.*;
 
-class Main {
-
-  public static String FirstReverse(String str) {
+class Main 
+{
+    public static String FirstReverse(String str) {
     return new StringBuilder().append(str).reverse().toString();
-    
-  }
+}
 
   public static void main (String[] args) {  
     // keep this function call here     
@@ -19,3 +17,6 @@ class Main {
   }
 
 }
+
+
+
